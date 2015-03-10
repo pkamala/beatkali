1779f4295bfe9eafb7ee034f4d7c1743ec62d0da
Peter <ricohombre88@gmail.com>
2015-03-10 15:48:17 +0300

    puccino2

--- app.js
+++ app.js
@@ -58,4 +58,9 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+var port = process.env.PORT || '3000';
+var server = app.listen(port, function()
+{
+    console.log('Listening on port '+ port);
+});
 
