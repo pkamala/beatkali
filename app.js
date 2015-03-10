08c918ebec4697cb9d3452e5b612bd96af8ba993
Peter <ricohombre88@gmail.com>
2015-03-10 20:02:18 +0300

    dfmwe

--- app.js
+++ app.js
@@ -54,6 +54,33 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+module.exports = function timeout(ms) {
+    ms = ms || 5000;
+
+    return function(req, res, next) {
+        var id = setTimeout(function(){
+            req.emit('timeout', ms);
+        }, ms);
+
+        req.on('timeout', function(){
+            if (res.headerSent) return debug('response started, cannot timeout');
+            var err = new Error('Response timeout');
+            err.timeout = ms;
+            err.status = 503;
+            next(err);
+        });
+
+        req.clearTimeout = function(){
+            clearTimeout(id);
+        };
+
+        res.on('header', function(){
+            clearTimeout(id);
+        });
+
+        next();
+    };
+};
 module.exports = app;
 var port = process.env.PORT || '3000';
 app.listen(port);
